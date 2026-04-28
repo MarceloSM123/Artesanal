6485688641e6afc706078dbbb82d5fb107d72ae8
MarceloSM123 <136668018+MarceloSM123@users.noreply.github.com>
2026-04-28 13:15:38 -0500

    5. solucion al nullPointerException

--- src/com/krakedev/artesanal/test/TestNegocio.java
+++ src/com/krakedev/artesanal/test/TestNegocio.java
@@ -7,13 +7,17 @@ public class TestNegocio {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Negocio negocio1 = new Negocio();
+		// Negocio negocio1 = new Negocio(); para evitar posibles nulos
+		// solucionamos el nullPointerException mediante el uso de constructores con parametros
+		Maquina nueva = new Maquina("Cerveza club", "Cerveza fría", 0.02, 8000);
+		Negocio negocio1 = new Negocio("Mi negocio", nueva);
 
 		System.out.println("Nombre : " + negocio1.getNombre());
 		System.out.println("Maquina : " + negocio1.getMaquinaA());
 
 		Maquina m1 = negocio1.getMaquinaA();
 		double capacidad = m1.getCapacidadMaxima();
+		
 
 	}
 
