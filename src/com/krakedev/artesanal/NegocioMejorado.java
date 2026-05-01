5bb7fe9b5d68f1686708140aae032920d2aa92b2
MarceloSM123 <136668018+MarceloSM123@users.noreply.github.com>
2026-04-30 20:09:33 -0500

    Parte 1.2 incializacion del arraylist en el constructor

--- src/com/krakedev/artesanal/NegocioMejorado.java
+++ src/com/krakedev/artesanal/NegocioMejorado.java
@@ -1,5 +1,21 @@
 package com.krakedev.artesanal;
 
-public class NegocioMejorado {
+import java.util.ArrayList;
 
+public class NegocioMejorado {
+	private ArrayList<Maquina> maquinas;
+
+	public NegocioMejorado() {
+        maquinas = new ArrayList<Maquina>();
+        
+    }
+	
+	public ArrayList<Maquina> getMaquinas() {
+		return maquinas;
+	}
+
+	public void setMaquinas(ArrayList<Maquina> maquinas) {
+		this.maquinas = maquinas;
+	}
+	
 }
