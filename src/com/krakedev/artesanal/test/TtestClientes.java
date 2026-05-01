1aae44b10924ecee7ddcdb08c9939c39e7e6d47c
MarceloSM123 <136668018+MarceloSM123@users.noreply.github.com>
2026-04-30 20:59:12 -0500

    Parte 2.11 Provocar Error, implementacion de la clase TestClientes

---
+++ src/com/krakedev/artesanal/test/TtestClientes.java
@@ -0,0 +1,15 @@
+package com.krakedev.artesanal.test;
+
+import com.krakedev.artesanal.NegocioMejorado;
+
+public class TtestClientes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		NegocioMejorado negocio = new NegocioMejorado();
+
+		negocio.registrarCliente("Juan Perez", "1234567890");
+
+	}
+
+}
